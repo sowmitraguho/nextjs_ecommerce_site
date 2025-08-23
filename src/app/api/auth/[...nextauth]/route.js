78beeb321b57758b5fa1002c1e40e17057de68f1
Sowmitra Guha <sowmmochy@gmail.com>
2025-08-23 09:08:07 +0600

    github login added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -43,10 +43,6 @@ export const authOptions = {
       return null
     }
   }),
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  }),
   GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
